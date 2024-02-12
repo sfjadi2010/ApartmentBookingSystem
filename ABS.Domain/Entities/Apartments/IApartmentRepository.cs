c7b1cf1792e4b9963bb86298deafda3584d301e3
Salman Farsi <salman.farsi@3together.us>
2024-02-11 22:48:34 -0500

    Added repository interfaces.

---
+++ ABS.Domain/Entities/Apartments/IApartmentRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABS.Domain.Entities.Apartments;
+
+public interface IApartmentRepository
+{
+    Task<Apartment> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+}
